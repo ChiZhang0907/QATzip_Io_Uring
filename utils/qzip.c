54695f7561ac0dae63735ee8da48a3a24f25b18f
ChiZhang0907 <chizhang0907@gmail.com>
2022-07-19 14:58:31 -0700

    minor fix

--- utils/qzip.c
+++ utils/qzip.c
@@ -554,7 +554,7 @@ void doProcessFile(QzSession_T *sess, const char *src_file_name,
         ret = QZ7Z_ERR_MALLOC;
         goto exit;
     }
-    if (g_read_o_direct) {
+    if (g_o_direct) {
         src_file = generateIoUringFile(src_file_name, O_RDONLY | O_DIRECT);
     } else {
         src_file = generateIoUringFile(src_file_name, O_RDONLY);
